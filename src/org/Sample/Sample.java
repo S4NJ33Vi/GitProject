99c2bd38f6975fc50d636103fb4cea3804fb6453
S4NJ33Vi <Sanjeevi1719@gmail.com>
2024-03-08 15:45:06 +0530

    Added few lines of code

---
+++ src/org/Sample/Sample.java
@@ -0,0 +1,15 @@
+package org.Sample;
+
+public class Sample {
+
+	private void add() {
+	System.out.println("Sample");
+}
+
+	public static void main(String[] args) {
+		
+		Sample s = new Sample();
+		s.add();
+	}
+	
+}
